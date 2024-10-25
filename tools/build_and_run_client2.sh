35ca761b12ccf510f1ee460bbcb3d968cfa03898
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-10-25 11:42:52 -0400

    chore: scripting

--- tools/build_and_run_client2.sh
+++ tools/build_and_run_client2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [[ "$OSTYPE" != "linux-gnu"* && "$OSTYPE" != "darwin"* ]]; then
+    echo "This script can only be run on Linux or macOS."
+    exit 1
+fi
+
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 cd "$SCRIPT_DIRECTORY"
